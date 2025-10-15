f2ecf1226674786af4b35842682041db4c98946d
Aibar <akenbay@icloud.com>
2025-10-15 22:51:44 +0500

    Add: flags, creating new client; Fix: go mod and go sum

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "flag"
+
 func main() {
+	cmd := flag.String("cmd", "block", "command: block or txs")
+	arg := flag.String("arg", "latest", "argument for command: latest or block number")
+	flag.Parse()
 
 }
